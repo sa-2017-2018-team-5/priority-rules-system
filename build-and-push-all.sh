dd31631adb5adb585dd120f22b2e4e5149c2c896
Antoine AUBÉ <aube.antoine@protonmail.com>
2017-10-25 23:54:24 +0200

    First attempt for creating docker image on the hub TG-30

--- build-and-push-all.sh
+++ build-and-push-all.sh
@@ -3,17 +3,25 @@
 # Administration client
 cd administration-client/docker
 ./build.sh
+./publish.sh
 
+echo ""
 cd ../..
 
 # Driver client
 cd driver-client/docker
 ./build.sh
+./publish.sh
 
+echo ""
 cd ../..
 
 # Backend
 cd backend/docker
 ./build.sh
+./publish.sh
 
+echo ""
 cd ../..
+
+echo "Build finished"
