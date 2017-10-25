580543e37c2a2fa21566f05fdac70241899a4e3b
danial <azswad20@gmail.com>
2017-10-26 00:34:59 +0200

    Communication messages TG-26

--- city-traffic-lights-orchestration/src/main/java/engine/ProcessMessageImpl.java
+++ city-traffic-lights-orchestration/src/main/java/engine/ProcessMessageImpl.java
@@ -49,29 +49,5 @@ public class ProcessMessageImpl implements ProcessMessage{
 
         return lightsID;
     }
-//
-//    @Override
-//    public JSONObject getRoute(JSONObject object) {
-//        return object.getJSONObject("route");
-//    }
-//
-//    @Override
-//    public JSONObject getCar(JSONObject object) {
-//        return object.getJSONObject("car");
-//    }
-//
-//    @Override
-//    public JSONArray getEncounteredLights(JSONObject object){
-//        return this.getRoute(object).getJSONArray("encounteredLights");
-//    }
-//
-//    @Override
-//    public List<String> getLightsID(JSONObject object) {
-//        List<String> trafficID = new ArrayList<>();
-//        JSONArray trafficLights = this.getEncounteredLights(object);
-//        for(int i = 0; i < trafficLights.length(); i++){
-//            trafficID.add(trafficLights.getJSONObject(i).getString("id"));
-//        }
-//        return trafficID;
-//    }
+
 }
