afe3a9a3aff925820690db097b4b5879d8b1f13e
Antoine AUBÉ <aube.antoine@protonmail.com>
2017-10-29 09:40:13 +0100

    Code smells

--- backend/src/main/java/fr/polytech/al/five/components/TrafficSupervisionClient.java
+++ backend/src/main/java/fr/polytech/al/five/components/TrafficSupervisionClient.java
@@ -3,6 +3,7 @@ package fr.polytech.al.five.components;
 import fr.polytech.al.five.TrafficSupervisor;
 import fr.polytech.al.five.entities.TrafficInformation;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
 import javax.ejb.Stateless;
@@ -15,26 +16,27 @@ import java.net.ConnectException;
 @Stateless
 public class TrafficSupervisionClient implements TrafficSupervisor {
 
+    private final static Logger LOGGER = Logger.getLogger(TrafficSupervisionClient.class);
+
     @Override
-    public TrafficInformation getTraffic(String road_id) {
-        // TODO: Connect to the external service.
+    public TrafficInformation getTraffic(String roadId) {
         int port = 9090;
         String host = "localhost";
         String serviceName = "/traffic-api/road/section=";
 
-        WebClient webClient = WebClient.create("http://" + host + ":" + port + serviceName + road_id)
+        WebClient webClient = WebClient.create("http://" + host + ":" + port + serviceName + roadId)
                 .accept(MediaType.APPLICATION_JSON)
                 .header("Content-Type", MediaType.APPLICATION_JSON)
                 .query("section", "request");
-        String rawResult;
 
         try {
-            rawResult = webClient.get(String.class);
-        } catch (Exception e){
-            return TrafficInformation.fromName("unknown");
-        }
+            JSONObject jsonResult = new JSONObject(webClient.get(String.class));
 
-        JSONObject jsonResult = new JSONObject(rawResult);
-        return TrafficInformation.fromName(jsonResult.getString("status").toLowerCase());
+            return TrafficInformation.valueOf(
+                    jsonResult.getString("status").toUpperCase());
+        } catch (Exception e){
+            LOGGER.error("Could not reach the external service.");
+            return TrafficInformation.UNKNOWN;
+        }
     }
 }
--- backend/src/main/java/fr/polytech/al/five/entities/TrafficInformation.java
+++ backend/src/main/java/fr/polytech/al/five/entities/TrafficInformation.java
@@ -4,34 +4,8 @@ package fr.polytech.al.five.entities;
  * @author Antoine Aubé (aube.antoine@protonmail.com)
  */
 public enum TrafficInformation {
-    // TODO: To be defined.
-    JAM("jam"), FREE("free"), UNKNOWN("unknow");
+    JAM,
+    FREE,
+    UNKNOWN;
 
-    private String name;
-
-    TrafficInformation(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Get name
-     * @return
-     */
-    public String getName() {
-        return this.name;
-    }
-
-    /**
-     *
-     * @param name
-     * @return a role from its name
-     */
-    public static TrafficInformation fromName(String name) {
-        for (TrafficInformation info : TrafficInformation.values()) {
-            if (info.name.equals(name)) {
-                return info;
-            }
-        }
-        return UNKNOWN;
-    }
 }
