8d07a28eb031095244b6b3d0c13d9a4e21382da3
Antoine AUBÉ <aube.antoine@protonmail.com>
2018-01-17 21:12:48 +0100

    Added default values for the driver client

--- driver-client/src/main/java/fr/polytech/al/five/runner/DriverInterface.java
+++ driver-client/src/main/java/fr/polytech/al/five/runner/DriverInterface.java
@@ -4,6 +4,8 @@ import asg.cliche.Command;
 import org.apache.log4j.Logger;
 import stubs.route.*;
 
+import java.util.Optional;
+
 /**
  * @author Antoine Aubé (aube.antoine@protonmail.com)
  */
@@ -11,12 +13,57 @@ public class DriverInterface {
 
     private static final Logger LOGGER = Logger.getLogger(DriverInterface.class);
 
+    private static Optional<Integer> getIntegerEnv(String reference) {
+        String variable = System.getenv(reference);
+
+        if (variable != null) {
+            try {
+                return Optional.of(Integer.parseInt(variable));
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<Float> getFloatEnv(String reference) {
+        String variable = System.getenv(reference);
+
+        if (variable != null) {
+            try {
+                return Optional.of(Float.parseFloat(variable));
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<String> getStringEnv(String reference) {
+        return Optional.ofNullable(System.getenv(reference));
+    }
+
     private Car car;
     private RouteWebService routeWebService;
 
     public DriverInterface(RouteWebService routeWebService) {
         car = new Car();
+        initialize();
+
         this.routeWebService = routeWebService;
+    }
+
+    private void initialize() {
+        getStringEnv("DEFAULT_CAR_TYPE").ifPresent(this::setType);
+        getIntegerEnv("DEFAULT_CAR_ID").ifPresent(this::setId);
+
+        Optional<Float> longitude = getFloatEnv("DEFAULT_CAR_LONGITUDE");
+        Optional<Float> latitude = getFloatEnv("DEFAULT_CAR_LATITUDE");
+        if (longitude.isPresent() && latitude.isPresent()) {
+            setPosition(longitude.get(), latitude.get());
+        }
     }
 
     @Command(name = "set-id")
