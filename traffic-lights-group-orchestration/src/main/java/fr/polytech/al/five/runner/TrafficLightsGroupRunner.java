6549840b578ae62a8428e1219db0bc7c3192c9fa
danial <azswad20@gmail.com>
2018-01-18 21:39:14 +0100

    Group config in config.properties, import using dockerfile

--- traffic-lights-group-orchestration/src/main/java/fr/polytech/al/five/runner/TrafficLightsGroupRunner.java
+++ traffic-lights-group-orchestration/src/main/java/fr/polytech/al/five/runner/TrafficLightsGroupRunner.java
@@ -1,45 +1,37 @@
 package fr.polytech.al.five.runner;
 
-import fr.polytech.al.five.actions.OnTrafficLightObservation;
 import fr.polytech.al.five.behaviour.PropertiesLoader;
 import fr.polytech.al.five.behaviour.TrafficLightsGroupState;
-import fr.polytech.al.five.bus.BusChannel;
 import fr.polytech.al.five.bus.BusInformation;
-import fr.polytech.al.five.bus.MessageConsumer;
-import fr.polytech.al.five.messages.TrafficLightObservationMessage;
 import org.apache.log4j.Logger;
-
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 public class TrafficLightsGroupRunner {
 
     private static final Logger LOGGER = Logger.getLogger(TrafficLightsGroupRunner.class);
 
     public static void main(String[] args) {
+
         String busAddress = System.getenv("BUS_ADDRESS");
         if (busAddress == null) {
             busAddress = "localhost";
         }
 
-        PropertiesLoader properties = new PropertiesLoader()
-                .setGroupId(System.getenv("TRAFFIC_GROUP_ID"))
-                .setTrafficLights(System.getenv("TRAFFIC_LIGHTS"))
-                .setTrafficRules(System.getenv("TRAFFIC_RULES"));
+        PropertiesLoader properties = new PropertiesLoader();
 
         BusInformation busInformation = new BusInformation(busAddress);
 
         TrafficLightsGroupState state = new TrafficLightsGroupState(properties);
 
-        // Set up the Traffic Lights Observation messages consumer.
-        MessageConsumer<TrafficLightObservationMessage> observationConsumer =
-                new MessageConsumer<>(busInformation);
-        try {
-            observationConsumer.makeConsume(BusChannel.TRAFFIC_LIGHT_OBSERVATION,
-                    new OnTrafficLightObservation(busInformation, state).getAction());
-        } catch (IOException | TimeoutException e) {
-            LOGGER.error("Error while connecting to the bus: " + e);
-            System.exit(1);
-        }
+        System.out.println(properties.toString());
+//        // Set up the Traffic Lights Observation messages consumer.
+//        MessageConsumer<TrafficLightObservationMessage> observationConsumer =
+//                new MessageConsumer<>(busInformation);
+//        try {
+//            observationConsumer.makeConsume(BusChannel.TRAFFIC_LIGHT_OBSERVATION,
+//                    new OnTrafficLightObservation(busInformation, state).getAction());
+//        } catch (IOException | TimeoutException e) {
+//            LOGGER.error("Error while connecting to the bus: " + e);
+//            System.exit(1);
+//        }
     }
 }
